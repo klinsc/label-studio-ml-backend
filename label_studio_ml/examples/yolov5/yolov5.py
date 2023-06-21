02ad42ee5f7a6947b4b3233c33419c8863064a02
klinsc <klinscea@gmail.com>
2023-06-21 10:52:20 +0200

    refactor(yolov5.py): change class name from MMDetection to yolov5 feat(yolov5.py): add support for yolov5 object detection model using hub.load() function from Ultralytics docs(yolov5.py): add comments to the code to improve readability

--- label_studio_ml/examples/yolov5/yolov5.py
+++ label_studio_ml/examples/yolov5/yolov5.py
@@ -19,63 +19,18 @@ from urllib.parse import urlparse
 logger = logging.getLogger(__name__)
 
 
-class MMDetection(LabelStudioMLBase):
-    """Object detector based on https://github.com/open-mmlab/mmdetection"""
+class yolov5(LabelStudioMLBase):
 
-    def __init__(self, config_file=None,
-                 checkpoint_file=None,
-                 image_dir=None,
-                 labels_file=None, score_threshold=0.3, device='cuda:0', **kwargs):
-        """
-        Load MMDetection model from config and checkpoint into memory.
-        (Check https://mmdetection.readthedocs.io/en/v1.2.0/GETTING_STARTED.html#high-level-apis-for-testing-images)
+    def __init__(self, **kwargs):
+        super(yolov5, self).__init__(**kwargs)
 
-        Optionally set mappings from COCO classes to target labels
-        :param config_file: Absolute path to MMDetection config file (e.g. /home/user/mmdetection/configs/faster_rcnn/faster_rcnn_r50_fpn_1x.py)
-        :param checkpoint_file: Absolute path MMDetection checkpoint file (e.g. /home/user/mmdetection/checkpoints/faster_rcnn_r50_fpn_1x_20181010-3d1b3351.pth)
-        :param image_dir: Directory where images are stored (should be used only in case you use direct file upload into Label Studio instead of URLs)
-        :param labels_file: file with mappings from COCO labels to custom labels {"airplane": "Boeing"}
-        :param score_threshold: score threshold to wipe out noisy results
-        :param device: device (cpu, cuda:0, cuda:1, ...)
-        :param kwargs: can contain endpoint_url in case of non amazon s3
-        """
-        super(MMDetection, self).__init__(**kwargs)
-        config_file = config_file or os.environ['config_file']
-        checkpoint_file = checkpoint_file or os.environ['checkpoint_file']
-        self.config_file = config_file
-        self.checkpoint_file = checkpoint_file
-        self.labels_file = labels_file
-        self.endpoint_url = kwargs.get('endpoint_url')
-        if self.endpoint_url:
-            logger.info(f'Using s3 endpoint url {self.endpoint_url}')
-        
-        # default Label Studio image upload folder
-        upload_dir = os.path.join(get_data_dir(), 'media', 'upload')
-        self.image_dir = image_dir or upload_dir
-        logger.debug(f'{self.__class__.__name__} reads images from {self.image_dir}')
-        if self.labels_file and os.path.exists(self.labels_file):
-            self.label_map = json_load(self.labels_file)
-        else:
-            self.label_map = {}
-
-        self.from_name, self.to_name, self.value, self.labels_in_config = get_single_tag_keys(
-            self.parsed_label_config, 'RectangleLabels', 'Image')
-        schema = list(self.parsed_label_config.values())[0]
-        self.labels_in_config = set(self.labels_in_config)
-
-        # Collect label maps from `predicted_values="airplane,car"` attribute in <Label> tag
-        self.labels_attrs = schema.get('labels_attrs')
-        if self.labels_attrs:
-            for label_name, label_attrs in self.labels_attrs.items():
-                for predicted_value in label_attrs.get('predicted_values', '').split(','):
-                    self.label_map[predicted_value] = label_name
-
-        print('Load new model from: ', config_file, checkpoint_file)
-        self.model = init_detector(config_file, checkpoint_file, device=device)
-        self.score_thresh = score_threshold
+        # Model
+        # or yolov5n - yolov5x6, custom
+        self.model = hub.load("ultralytics/yolov5", "yolov5s")
 
     def _get_image_url(self, task):
-        image_url = task['data'].get(self.value) or task['data'].get(DATA_UNDEFINED_NAME)
+        image_url = task['data'].get(
+            self.value) or task['data'].get(DATA_UNDEFINED_NAME)
         if image_url.startswith('s3://'):
             # presign s3 url
             r = urlparse(image_url, allow_fragments=False)
@@ -88,16 +43,17 @@ class MMDetection(LabelStudioMLBase):
                     Params={'Bucket': bucket_name, 'Key': key}
                 )
             except ClientError as exc:
-                logger.warning(f'Can\'t generate presigned URL for {image_url}. Reason: {exc}')
+                logger.warning(
+                    f'Can\'t generate presigned URL for {image_url}. Reason: {exc}')
         return image_url
 
     def predict(self, tasks, **kwargs):
         assert len(tasks) == 1
         task = tasks[0]
-        
+
         image_url = self._get_image_url(task)
         image_path = self.get_local_path(image_url)
-        
+
         model_results = inference_detector(self.model, image_path)
         results = []
         all_scores = []
